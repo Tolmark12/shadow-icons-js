71a09af56ba4d5f666b8a976cdf6905dce712938
Mark Parson <mark@delorum.com>
2016-03-28 15:31:06 -0600

    BUILD - 0.0.80

--- rel/app.js
+++ rel/app.js
@@ -23,62 +23,70 @@ ShadowIcons = (function() {
   };
 
   ShadowIcons.prototype.replacePlaceholdersWithSVGs = function(svg, $jqueryContext) {
-    var $g, $holder, $svg, $targetSvg, id, image, images, lockToMax, modBox, newNode, rawHtml, scalable, serializer, size, usesSymbols, _i, _len, _ref, _ref1, _ref2, _ref3, _results;
+    var $svg, image, images, _i, _len, _results;
     $svg = $(this.buildSvg(svg, "main"));
     images = $("img.shadow-icon", $jqueryContext);
     _results = [];
     for (_i = 0, _len = images.length; _i < _len; _i++) {
       image = images[_i];
-      id = $(image).attr("data-src");
-      scalable = ((_ref = $(image).attr("scalable")) != null ? _ref.toUpperCase() : void 0) === 'TRUE';
-      lockToMax = ((_ref1 = $(image).attr("lock-to-max")) != null ? _ref1.toUpperCase() : void 0) === 'TRUE';
-      lockToMax || (lockToMax = ((_ref2 = $(image).attr("data-lock-to-max")) != null ? _ref2.toUpperCase() : void 0) === 'TRUE');
-      scalable || (scalable = ((_ref3 = $(image).attr("data-scalable")) != null ? _ref3.toUpperCase() : void 0) === 'TRUE');
-      $g = $("#" + id, $svg);
-      size = $g.attr("data-size").split('x');
-      $g.removeAttr("data-size");
-      modBox = {
-        width: size[0],
-        height: size[1]
-      };
-      $targetSvg = $g[0];
-      usesSymbols = false;
-      if ($targetSvg == null) {
-        _results.push(console.error("Shadow Icons : Tried to add an SVG with the id '" + id + "', but a SVG with id doesn't exist in the library SVG."));
-      } else {
-        serializer = new XMLSerializer();
-        rawHtml = serializer.serializeToString($targetSvg);
-        if (usesSymbols) {
-          newNode = $(this.buildSvg(rawHtml, id, pxSymbolString));
-        } else {
-          newNode = $(this.buildSvg(rawHtml, id));
-        }
-        $('body').append(newNode);
-        if (scalable) {
-          newNode.get(0).setAttribute("viewBox", "0 0 " + modBox.width + " " + modBox.height);
-          $holder = $("<div class='holder'><div>");
-          $holder.css({
-            "width": "100%",
-            "display": "inline-block"
-          });
-          if (lockToMax) {
-            $holder.css({
-              "max-width": "" + modBox.width + "px",
-              "max-height": "" + modBox.height + "px"
-            });
-          }
-          $holder.append(newNode);
-          _results.push($(image).replaceWith($holder));
-        } else {
-          newNode.attr({
-            width: "" + modBox.width + "px",
-            height: "" + modBox.height + "px"
-          });
-          _results.push($(image).replaceWith(newNode));
-        }
-      }
+      _results.push(this.createSvg(image, $svg));
     }
     return _results;
+  };
+
+  ShadowIcons.prototype.createSvg = function(image, $svg) {
+    var $g, $holder, $targetSvg, id, lockToMax, modBox, newNode, rawHtml, scalable, serializer, size, usesSymbols, _ref, _ref1, _ref2, _ref3;
+    id = $(image).attr("data-src");
+    scalable = ((_ref = $(image).attr("scalable")) != null ? _ref.toUpperCase() : void 0) === 'TRUE';
+    lockToMax = ((_ref1 = $(image).attr("lock-to-max")) != null ? _ref1.toUpperCase() : void 0) === 'TRUE';
+    lockToMax || (lockToMax = ((_ref2 = $(image).attr("data-lock-to-max")) != null ? _ref2.toUpperCase() : void 0) === 'TRUE');
+    scalable || (scalable = ((_ref3 = $(image).attr("data-scalable")) != null ? _ref3.toUpperCase() : void 0) === 'TRUE');
+    $g = $("#" + id, $svg);
+    if ($g[0] == null) {
+      console.log("Shadow Icons : Tried to add an SVG with the id '" + id + "', but an SVG with id doesn't exist in the library SVG.");
+      return;
+    } else if ($g.attr("data-size") == null) {
+      console.log("Unable to find the size attribute on '" + id + "'");
+      return;
+    }
+    size = $g.attr("data-size").split('x');
+    $g.removeAttr("data-size");
+    modBox = {
+      width: size[0],
+      height: size[1]
+    };
+    $targetSvg = $g[0];
+    usesSymbols = false;
+    serializer = new XMLSerializer();
+    rawHtml = serializer.serializeToString($targetSvg);
+    if (usesSymbols) {
+      newNode = $(this.buildSvg(rawHtml, id, pxSymbolString));
+    } else {
+      newNode = $(this.buildSvg(rawHtml, id));
+    }
+    $('body').append(newNode);
+    if (scalable) {
+      newNode.get(0).setAttribute("viewBox", "0 0 " + modBox.width + " " + modBox.height);
+      $holder = $("<div class='holder'><div>");
+      $holder.css({
+        "width": "100%",
+        "display": "inline-block"
+      });
+      if (lockToMax) {
+        $holder.css({
+          "max-width": "" + modBox.width + "px",
+          "max-height": "" + modBox.height + "px"
+        });
+      }
+      $holder.append(newNode);
+      return $(image).replaceWith($holder);
+    } else {
+      newNode.attr({
+        width: "" + modBox.width + "px",
+        height: "" + modBox.height + "px"
+      });
+      return $(image).replaceWith(newNode);
+    }
   };
 
   ShadowIcons.prototype.buildSvg = function(svgSubElement, id, symbols) {
